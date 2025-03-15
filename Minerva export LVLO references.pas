0421ffb062e9b15f1f2eebaae41a3fbfa9a09a24
snooptodd <snooptodd@gmail.com>
2025-03-15 12:43:30 -0400

    removed deeply nested loops

--- Minerva export LVLO references.pas
+++ Minerva export LVLO references.pas
@@ -1,125 +1,96 @@
+{
+  Generate csv file of form ID list contents
+  export Leveled lists LVLO entries
+	idealy this will loop through the first leveled list and get elements from the sub leveled lists its not there yet in fact it is barely usable
+	Version: 0.1
+	run the folloing to clean up the output. ya this could be done in the script. I barely understand what the script is doing and dont want to break it.
+	sed dmp_minerva.txt -e 's/BS02_SpecialVendor_Minerva_LL._GoldVendor.//g' -e 's/.\[\w*:\w*\].//g' -e 's/ .*Plan: //' -e 's/"/|/' |sort //"
+	Version 0.1.1
+	the output has been cleaned up and gold values added. 
+	NOTE: Gold value does not have Minerva's discount applied
 
-	//export Leveled lists LVLO entries
-	//idealy this will loop through the first leveled list and get elements from the sub leveled lists its not there yet in fact it is barely usable
-	//Version: 0.1
-	//run the folloing to clean up the output. ya this could be done in the script. I barely understand what the script is doing and dont want to break it.
-	//sed dmp_minerva.txt -e 's/BS02_SpecialVendor_Minerva_LL._GoldVendor.//g' -e 's/.\[\w*:\w*\].//g' -e 's/ .*Plan: //' -e 's/"/|/' |sort //"
-	// Version 0.1.1
-	// the output has been cleaned up and gold values added. 
-	// NOTE: Gold value does not have Minerva's discount applied
-	// Inventory list ID | Plan Name | gold value 
-	// 01|Farmable Dirt Tiles|50
-	// 01|Sympto-Matic|2000|
+	version 0.2.0
+	used scratchy's script as a base to get rid of the deeply nested loops
 
-	// TODO get this thing to start from formid 005FFDEB and follow the trail 
+  
+  Inventory list ID | Plan Name | gold value 
+	01|Farmable Dirt Tiles|50
+	01|Sympto-Matic|2000|
 
-
+	TODO get this thing to start from formid 005FFDEB and follow the trail 
+    
+}
 unit UserScript;
 
-const
-  FormIDStart = $005FFDEB; // starting formid
-  FormIDCount = 1;        // formids to list
+var // Initialize variables
+  slExport: TStringList;
+  currentItemEDID, filename, value, Minerva_Inventory_ListID, FULLname, GoldBullionValue, GoldBullionValueGLOB, s, valueType: string;
+  Minerva_LeveledListEntries, Minerva_CurrentLeveledListEntry, Minerva_CurrentLeveledListRecord, Minerva_CurrentLVLO, Minerva_CurrentReference : IInterface;
+  Minerva_Inventory_LeveledListEntries, Minerva_Inventory_currentLeveledListEntry, Minerva_Inventory_currentLeveledListRecord, Minerva_Inventory_currentLVLO, Minerva_Inventory_currentReference : IInterface;
+  i, j: integer;
 
-
-var
-	slParentElements: TStringList;
-	sTargetLevel: string;
-	bDebug: bool;
-	slExport: TStringList;
-
-//============================================================================
 function Initialize: integer;
+begin
+  slExport := TStringList.Create;
+end;
 
-	begin
-
-		bDebug := true;
-
-		slExport := TStringList.Create;
-
-		slParentElements := TStringList.Create;
-
-		if bDebug then begin
-			ClearMessages();
-			AddMessage('Applying script...' + #13#10);
-		end;
-
-	end;
-
-//============================================================================
 function Process(e: IInterface): integer;
+begin
+  // filename := EditorID(e); // Get EDID for filename
+  // slExport.Add(IntToHex(FormID(e) and $FFFFFF, 8) + ',' + EditorID(e)); // Add selected record's FormID & EDID to top of CSV
+  // slExport.Add('FormID,EDID,FULL'); //Row headers
+  Minerva_LeveledListEntries := ElementByPath(e, 'Leveled List Entries'); // Get array of Leveled List entries https://ptb.discord.com/channels/471930020454072348/483466910571167746/1101658577023803442
+  for i := 0 to ElementCount(Minerva_LeveledListEntries) - 1 do begin // For each FormID entry
+    // Minerva main record
+    // LVLI \ Leveled List Entries \ Leveled List Entry \ LVLO - LVLO \ Reference
+    Minerva_CurrentLeveledListEntry := ElementByIndex(Minerva_LeveledListEntries, i); // Grab i'th Leveled list Entry
+    Minerva_CurrentLVLO := ElementBySignature(Minerva_CurrentLeveledListEntry,'LVLO');
+    Minerva_CurrentReference := ElementByName(Minerva_CurrentLVLO,'Reference');
+    Minerva_CurrentLeveledListRecord := LinksTo(Minerva_CurrentReference); // Get referenced record's mainRecordElement
+    // this should be the LVLO/reference that is in the i'th Leveled list entry
+    Minerva_Inventory_LeveledListEntries := ElementByPath(Minerva_CurrentLeveledListRecord, 'Leveled List Entries'); // Get array of Leveled List entries https://ptb.discord.com/channels/471930020454072348/483466910571167746/1101658577023803442
+    for j := 0 to ElementCount(Minerva_Inventory_LeveledListEntries) - 1 do begin // For each FormID entry
+      // Minerva linked records 
+      // LVLI \ Leveled List Entries \ Leveled List Entry \ LVLO - LVLO \ Reference
+      Minerva_Inventory_currentLeveledListEntry := ElementByIndex(Minerva_Inventory_LeveledListEntries, j); // Grab i'th Leveled list Entry
+      Minerva_Inventory_currentLVLO := ElementBySignature(Minerva_Inventory_currentLeveledListEntry,'LVLO');
+      Minerva_Inventory_currentReference := ElementByName(Minerva_Inventory_currentLVLO,'Reference');
+      Minerva_Inventory_currentLeveledListRecord := LinksTo(Minerva_Inventory_currentReference); // Get referenced record's mainRecordElement
+      // this should be the LVLO/reference that is in the i'th Leveled list entry
+      //LVLI \ Leveled List Entries \ Leveled List Entry \ Conditions \ Condition \ CTDA - CTDA \ Comparison Value
+      //Minerva_Inventory_ListID := GetElementEditValues(ElementBySignature(ElementByName(ElementByName(Minerva_CurrentLeveledListEntry,'Conditions'),'Condition'),'CTDA'),'Comparison Value'); 
+      Minerva_Inventory_ListID := delete(GetElementEditValues(Minerva_CurrentLeveledListRecord, 'EDID'),1,42);
+      FULLname := delete(GetElementEditValues(Minerva_Inventory_currentLeveledListRecord , 'FULL'),1,6);
+      GoldBullionValue :=  GetElementEditValues(ElementBySignature(Minerva_Inventory_currentLeveledListRecord, 'DATA'), 'Value');
+      GoldBullionValueGLOB := GetElementEditValues(LinksTo(ElementBySignature(Minerva_Inventory_currentLeveledListRecord, 'BVGO')), 'FLTV');
+      if pos('M',Minerva_Inventory_ListID) = 0 then begin
+        // if GoldBullionValueGLOB has a value then that is used for the gold value of the item
+        if GoldBullionValueGLOB = '' then 
+          s := Minerva_Inventory_ListID + '|' + FULLname + '|' + GoldBullionValue
+        else 
+          s := Minerva_Inventory_ListID + '|' + FULLname + '|' + StringReplace(GoldBullionValueGLOB,'.000000','',[rfReplaceAll, rfIgnoreCase]);
+      end;
+      // AddMessage(// Add line of text to the export
+      // IntToHex(FormID(Minerva_Inventory_currentLeveledListRecord) and $FFFFFF, 8) + ',' + // Add FormID
+      // Minerva_Inventory_ListID + ',' + // Add EDID
+      // // name(Minerva_Inventory_currentLVLO) + ',' + // Add EDID
+      // Name(Minerva_CurrentLeveledListRecord) + ',' + // Add FULL)
+      // Name(Minerva_Inventory_currentLeveledListRecord) // Add FULL)
+      // );
+      AddMessage(s);
+      s := '';
+      // slExport.add(s);
 
-	var
-		h, i, j, k, l: integer;
-		p, r, L0, L1, L2, L3, L4: IInterface;
-		s, FULLname, GoldBullionValue, GoldBullionValueGLOB,EditorID,MinervaInventoryListID : string;
+    end;
+  end;
+end;
 
-	begin
-
-		slParentElements.Clear;
-		p := GetFile(e);
-		r := RecordByFormID(p, FormIDStart, True); //change 'e' to 'r' to not use UI selected records
-		// signature check
-		if (Signature(e) = 'LVLI') or (Signature(e) = 'LVLN') or (Signature(e) = 'LVSP') then
-			slParentElements.Add('Leveled List Entries')
-		else
-			exit;
-		// Leveled List 
-		// -------------------------------------------------------------------------------
-		for h := 0 to slParentElements.Count - 1 do begin
-
-			// level 1 Leveled List Entries
-			L0 := ElementByPath(e, slParentElements[h]);
-			
-			// -------------------------------------------------------------------------------
-			for i := 0 to ElementCount(L0) - 1 do begin
-
-				// level 2 Leveled List Entry
-				L1 := ElementByIndex(L0, i);
-				
-				// -------------------------------------------------------------------------------
-				for j := 0 to ElementCount(L1) - 1 do begin
-
-					// level 3 LVLO - LVLO
-					L2 := ElementByIndex(L1, j);
-					//
-					if CompareStr(Signature(L2), 'LVLO') = 0 then begin
-						// LVLI \ Leveled List Entries \ Leveled List Entry \ LVLO - LVLO \ Reference
-						// ElementByName(L2, 'Reference') gets element of LVLO named 'Reference'
-						// LinksTo(ElementByName(L2, 'Reference')) 'opens' the record 
-						L3 := LinksTo(ElementByName(L2, 'Reference')); // Reference
-						MinervaInventoryListID := delete(GetElementEditValues(e, 'EDID'),1,42);
-						//if CompareStr(Signature(L3), 'BOOK') = 0 then begin
-							FULLname := delete(GetElementEditValues(L3 , 'FULL'),1,6);
-							GoldBullionValue :=  GetElementEditValues(ElementBySignature(L3, 'DATA'), 'Value');
-							GoldBullionValueGLOB := GetElementEditValues(LinksTo(ElementBySignature(L3, 'BVGO')), 'FLTV');
-							// if GoldBullionValueGLOB has a value then that is used for the gold value of the item
-							if GoldBullionValueGLOB = '' then 
-								s := MinervaInventoryListID + '|' + FULLname + '|' + GoldBullionValue
-							else 
-								s := MinervaInventoryListID + '|' + FULLname + '|' + StringReplace(GoldBullionValueGLOB,'.000000','',[rfReplaceAll, rfIgnoreCase]);
-							//AddMessage(s);
-							slExport.Add(s);
-						//end; // end if
-					end; // end if
-
-				end; // end for - level 3
-
-			end; // end for - level 2
-
-		end; // end for - level 1
-
-		slParentElements.Clear;
-
-	end;
-
-//============================================================================
 function Finalize: integer;
 var
   dlgSave: TSaveDialog;
   ExportFileName: string;
 begin
   if slExport.Count <> 0 then begin
-    //slExport.sort;
     dlgSave := TSaveDialog.Create(nil);
     try
       dlgSave.Options := dlgSave.Options + [ofOverwritePrompt];
